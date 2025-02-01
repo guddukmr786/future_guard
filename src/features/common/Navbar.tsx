a561beb2ad45ebedb4c31cf32bcd456e86e18441
Guddu <guddukmr786@gmail.com>
2025-02-01 22:31:16 +0530

    remove the logout options

--- src/features/common/Navbar.tsx
+++ src/features/common/Navbar.tsx
@@ -7,9 +7,9 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
+// import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
+// import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import SecurityIcon from '@mui/icons-material/Security';
 import { useNavigate } from 'react-router-dom';
@@ -21,26 +21,26 @@ const pages = [
   { "label": "Services", "path": "/services" },
   { "label": "Contact", "path": "/contact-us" }
 ]
-const settings = ['Profile', 'Logout'];
+// const settings = ['Profile', 'Logout'];
 
 function Navbar() {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  // const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
   const navigate = useNavigate();
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
+  // const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+  //   setAnchorElUser(event.currentTarget);
+  // };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+  // const handleCloseUserMenu = () => {
+  //   setAnchorElUser(null);
+  // };
   const handleNavigate = (path: string) => {
     navigate(path)
   }
--- src/features/contact/Contact.tsx
+++ src/features/contact/Contact.tsx
@@ -138,7 +138,7 @@ const Contact: React.FC = () => {
 
                 {/* Embedded Google Map */}
                 <Box sx={{ height: '300px', borderRadius: 2, overflow: 'hidden' }}>
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3598.568381066123!2d85.18531707485016!3d25.586022815843116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed58aae91ee8af%3A0x3feea4993233e9d7!2sZero%20Mile!5e0!3m2!1sen!2sin!4v1738427943282!5m2!1sen!2sin" width="100%" height="100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3598.568381066123!2d85.18531707485016!3d25.586022815843116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed58aae91ee8af%3A0x3feea4993233e9d7!2sZero%20Mile!5e0!3m2!1sen!2sin!4v1738427943282!5m2!1sen!2sin" width="100%" height="100%" loading="lazy" ></iframe>
                 </Box>
               </CardContent>
             </Card>
